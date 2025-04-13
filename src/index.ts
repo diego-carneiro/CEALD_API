63cec75d0e4700c49e315f0dd80562a7be6dfb93
Diego Carneiro <diego-linux@Acer-Diego>
2025-04-13 14:00:13 -0300

    CORS config

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ config();
 
 const app = express();
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://ceald-web-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Conectar ao MongoDB
