f46874bcb4c2957cb8c93dd673f7f69efcc9faee
Diego Carneiro <diego-linux@Acer-Diego>
2025-04-16 01:52:06 -0300

    fix fix

--- dist/repositories/guestsRepository.js
+++ dist/repositories/guestsRepository.js
@@ -4,33 +4,45 @@ exports.CreateGuestRepository = exports.GetGuestsRepository = void 0;
 const mongo_1 = require("../database/mongo");
 class GetGuestsRepository {
     async getGuests() {
-        const guests = await mongo_1.MongoClient.db
-            .collection("CEALD_Guests")
-            .find({})
-            .toArray();
-        return guests.map(({ _id, ...rest }) => ({
-            ...rest,
-            id: _id.toHexString(),
-        }));
+        try {
+            const guests = await mongo_1.MongoClient.db
+                .collection("CEALD_Guests")
+                .find({})
+                .toArray();
+            return guests.map(({ _id, ...rest }) => ({
+                ...rest,
+                id: _id.toHexString(),
+            }));
+        }
+        catch (error) {
+            console.error("Guest not registered:", error);
+            throw error;
+        }
     }
 }
 exports.GetGuestsRepository = GetGuestsRepository;
 class CreateGuestRepository {
     async createGuest(params) {
-        const { insertedId } = await mongo_1.MongoClient.db
-            .collection("CEALD_Guests")
-            .insertOne(params);
-        const guest = await mongo_1.MongoClient.db
-            .collection("CEALD_Guests")
-            .findOne({ _id: insertedId });
-        if (!guest) {
-            throw new Error("Guest not registered");
+        try {
+            const { insertedId } = await mongo_1.MongoClient.db
+                .collection("CEALD_Guests")
+                .insertOne(params);
+            const guest = await mongo_1.MongoClient.db
+                .collection("CEALD_Guests")
+                .findOne({ _id: insertedId });
+            if (!guest) {
+                throw new Error("Guest not registered");
+            }
+            const position = (await mongo_1.MongoClient.db
+                .collection("CEALD_Guests")
+                .countDocuments({ _id: { $lt: insertedId } })) + 1;
+            const { _id, ...rest } = guest;
+            return { id: _id.toHexString(), ...rest, position };
         }
-        const position = (await mongo_1.MongoClient.db
-            .collection("CEALD_Guests")
-            .countDocuments({ _id: { $lt: insertedId } })) + 1;
-        const { _id, ...rest } = guest;
-        return { id: _id.toHexString(), ...rest, position };
+        catch (error) {
+            console.error("Guest not registered:", error);
+            throw error;
+        }
     }
 }
 exports.CreateGuestRepository = CreateGuestRepository;
